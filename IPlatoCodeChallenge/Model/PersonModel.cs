4f5eb2912296d4a21ea371fad46e7f2be933008c
Simon Entwisle <bibiplone@hotmail.com>
2022-06-27 15:15:17 +0100

    Add project files.

---
+++ IPlatoCodeChallenge/Model/PersonModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPlatoCodeChallenge.Model
+{
+    public class PersonModel { }
+
+    public class Person : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private string dob;
+        private string profession;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    RaisePropertyChanged("FirstName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    RaisePropertyChanged("LastName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string DOB
+        {
+            get { return dob; }
+
+            set
+            {
+                if (lastName != value)
+                {
+                    dob = value;
+                    RaisePropertyChanged("LastName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string Profession
+        {
+            get { return profession; }
+
+            set
+            {
+                if (lastName != value)
+                {
+                    profession = value;
+                    RaisePropertyChanged("LastName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ IPlatoCodeChallenge/ViewModel/PersonViewModel.cs
@@ -0,0 +1,62 @@
+﻿using IPlatoCodeChallenge.Model;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System;
+using IPlatoCodeChallenge;
+
+namespace IPlatoCodeChallenge.ViewModel
+{
+
+    public class PersonViewModel
+    {
+        public GenericICommand DeleteCommand { get; set; }
+
+        public PersonViewModel()
+        {
+            LoadPeople();
+            DeleteCommand = new GenericICommand(OnDelete, CanDelete);
+        }
+
+        public ObservableCollection<Person> People
+        {
+            get;
+            set;
+        }
+
+        public void LoadPeople()
+        {
+            ObservableCollection<Person> people = new ObservableCollection<Person>();
+            people.Add(new Person { FirstName = "Mark", LastName = "Allain", DOB = "04/10/1972", Profession = "Brick Layer" });
+            people.Add(new Person { FirstName = "Allen", LastName = "Brown", DOB = "10/06/2002", Profession = "Comedian" });
+            people.Add(new Person { FirstName = "Linda", LastName = "Hamerski", DOB = "22/07/1984", Profession = "Gardenner" });
+
+            People = people;
+        }
+
+        private Person _selectedPerson;
+
+        public Person SelectedPerson
+        {
+            get
+            {
+                return _selectedPerson;
+            }
+
+            set
+            {
+                _selectedPerson = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private void OnDelete()
+        {
+           People.Remove(SelectedPerson);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedPerson != null;
+        }
+    }
+}
